df208b3702eea2c34409b1a88bb27cb0c883f327
TheStarfallIsInfinite <nielgargan@gmail.com>
2026-05-14 22:06:37 +0200

    Update antidirect.lua

--- modules/antidirect.lua
+++ modules/antidirect.lua
@@ -1,21 +1,32 @@
--- [[ 67 HUB - MODULE: antidirect.lua ]]
+-- [[ 67 HUB - MOBILE SAFE ANTI-AFK ]]
 _G.autoJumping = true
 
 local VIM = game:GetService("VirtualInputManager")
 local Workspace = game:GetService("Workspace")
 
 task.spawn(function()
+    -- 1. WAIT 60 SECONDS BEFORE THE VERY FIRST CLICK
+    -- This gives you time to close the menu or move around first
+    task.wait(60)
+
     while true do
         if _G.autoJumping then
             local viewportSize = Workspace.CurrentCamera.ViewportSize
-            -- Offset from center to avoid game buttons in image_75e978.png
-            local clickX = (viewportSize.X / 2) + 100 
-            local clickY = (viewportSize.Y / 2) + 100
+            
+            -- 2. CLICK BOTTOM RIGHT SAFE ZONE
+            -- Avoids center buttons and top-left menu icons seen in your photo
+            local clickX = viewportSize.X * 0.9 
+            local clickY = viewportSize.Y * 0.9
 
+            -- Simulate Click
             VIM:SendMouseButtonEvent(clickX, clickY, 0, true, game, 1)
             task.wait(0.1)
             VIM:SendMouseButtonEvent(clickX, clickY, 0, false, game, 1)
+            
+            print("67 HUB: Safe-zone click executed to prevent kick.")
         end
+        
+        -- 3. WAIT 10 MINUTES UNTIL NEXT CLICK
         task.wait(600)
     end
 end)
