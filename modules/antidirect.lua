de96cb3a80e0c61ee22389b7cff39521267abb91
TheStarfallIsInfinite <nielgargan@gmail.com>
2026-05-14 22:02:57 +0200

    Update antidirect.lua

--- modules/antidirect.lua
+++ modules/antidirect.lua
@@ -1,41 +1,21 @@
--- [[ 67 HUB - ANTI-AFK AUTOCLICKER ]]
+-- [[ 67 HUB - MODULE: antidirect.lua ]]
 _G.autoJumping = true
 
 local VIM = game:GetService("VirtualInputManager")
 local Workspace = game:GetService("Workspace")
 
--- Function to send a notification through the UI
-local function notifyClick()
-    -- We use pcall just in case Rayfield isn't fully initialized when the loop runs
-    pcall(function()
-        local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-        Rayfield:Notify({
-            Title = "Anti-AFK System",
-            Content = "Center click executed. Session kept active!",
-            Duration = 5,
-            Image = 4483362458,
-        })
-    end)
-end
-
 task.spawn(function()
     while true do
         if _G.autoJumping then
-            -- Get the center of the screen
             local viewportSize = Workspace.CurrentCamera.ViewportSize
-            local centerX = viewportSize.X / 2
-            local centerY = viewportSize.Y / 2
+            -- Offset from center to avoid game buttons in image_75e978.png
+            local clickX = (viewportSize.X / 2) + 100 
+            local clickY = (viewportSize.Y / 2) + 100
 
-            -- Click
-            VIM:SendMouseButtonEvent(centerX, centerY, 0, true, game, 1)
+            VIM:SendMouseButtonEvent(clickX, clickY, 0, true, game, 1)
             task.wait(0.1)
-            VIM:SendMouseButtonEvent(centerX, centerY, 0, false, game, 1)
-            
-            -- Show the notification instead of printing
-            notifyClick()
+            VIM:SendMouseButtonEvent(clickX, clickY, 0, false, game, 1)
         end
-        
-        -- Wait 10 minutes (600 seconds)
         task.wait(600)
     end
 end)
